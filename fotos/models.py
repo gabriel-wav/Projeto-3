3d681f89449c031e681184f4b133e43fb7814d59
gabriel-wav <gabfer.silva@gmail.com>
2025-08-27 21:31:03 -0300

    ainda nao funciona 2

--- fotos/models.py
+++ fotos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Foto(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='fotos/')
+    image = models.ImageField(upload_to='galeria/img')
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

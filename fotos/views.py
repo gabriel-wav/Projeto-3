97066db851a2154a5f39f83f0359443d55ea35a9
gabriel-wav <gabfer.silva@gmail.com>
2025-08-28 19:21:56 -0300

    site funcional

--- fotos/views.py
+++ fotos/views.py
@@ -3,19 +3,20 @@ from .models import Foto
 
 # Create your views here.
 def home(request):
-    return render(request, 'fotos/home.html')
+    fotos = Foto.objects.all()
+    return render(request, 'fotos/home.html', {'fotos': fotos})
 
 def foto_detail(request, id):
     foto = get_object_or_404(Foto, id=id)
     context = {'foto': foto}
     return render(request, 'fotos/foto_detail.html', context)
 
-def pesquisar_fotos(request): 
+def pesquisar_fotos(request):
     query = request.GET.get("q")
     resultados = []
-    
+
     if query:
         resultados = Foto.objects.filter(title__icontains=query)
-        
+
     context = {'query': query, 'resultados': resultados}
     return render(request, 'fotos/pesquisar_fotos.html', context)
